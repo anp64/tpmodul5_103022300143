16c8273cfb01fb45baff543309666fa40b68642a
ASUS <ASUS@LAPTOP-85PHJKK7>
2025-03-16 14:12:30 +0700

    Add project files.

---
+++ tpmodul5_103022300143/HaloGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tpmodul5_103022300143
+{
+    public class HaloGeneric
+    {
+        public static void sapaUser<T>(T user)
+        {
+            Console.WriteLine($"halo user {user}");
+        }
+    }
+}
---
+++ tpmodul5_103022300143/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using tpmodul5_103022300143;
+
+class program
+{
+    static void Main()
+    {
+        HaloGeneric.sapaUser("nama");
+
+        DataGeneric<string> data = new DataGeneric<string>("111");
+        data.PrintData();
+    }
+}
